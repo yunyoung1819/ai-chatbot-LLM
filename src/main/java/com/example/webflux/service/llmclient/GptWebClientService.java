e9be2c13d46106fe1b3b2421ecf3d0c222278e9c
yunyoung1819 <yunyoung1819@hanmail.net>
2026-01-10 18:34:15 +0900

    feat: GptWebClientService 구현

--- src/main/java/com/example/webflux/service/llmclient/GptWebClientService.java
+++ src/main/java/com/example/webflux/service/llmclient/GptWebClientService.java
@@ -4,6 +4,7 @@ import com.example.webflux.model.llmclient.LlmChatRequestDto;
 import com.example.webflux.model.llmclient.LlmChatResponseDto;
 import com.example.webflux.model.llmclient.LlmType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 public class GptWebClientService implements LlmWebClientService {
 
     private final WebClient webClient;
+    @Value("${llm.gpt.key}")
     private String getApiKey;
 
     @Override
---
+++ src/main/java/com/example/webflux/service/llmclient/gpt/GptChatRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.webflux.service.llmclient.gpt;
+
+import java.io.Serial;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GptChatRequestDto implements Serializable {
+
+}
